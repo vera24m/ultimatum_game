ac7cc3a040fc10e80d1e540d42c8a2cc2d06bb1d
Vera Matei <vera24m@yahoo.com>
2013-10-14 23:09:39 +0200

    Add round pages, store player ID in session, create Kind model.

--- game/models.py
+++ game/models.py
@@ -1,19 +1,27 @@
 from django.db import models
 
-class Opponent(models.Model):
-    HUMAN = 'h'
-    COMPUTER = 'c'
-    ROBOT = 'r'
-    KIND = (
-        (HUMAN, 'Human'),
-        (COMPUTER, 'Computer'),
-        (ROBOT, 'Robot'),
+class Kind(models.Model):
+    ID_HUMAN = 'h'
+    ID_COMPUTER = 'c'
+    ID_ROBOT = 'r'
+    IDS = (
+        (ID_HUMAN, 'Human'),
+        (ID_COMPUTER, 'Computer'),
+        (ID_ROBOT, 'Robot'),
     )
 
-    # XXX: Manually set ID/PK.
+    id = models.CharField(max_length=1, choices=IDS, primary_key=True)
 
-    kind = models.CharField(max_length=1, choices=KIND)
+    def __unicode__(self):
+        return self.get_id_display()
+
+# XXX: Is there a better way to ensure existence of all Kinds?
+for k in Kind.IDS:
+    Kind.objects.get_or_create(id=k[0])
+
+class Opponent(models.Model):
     # XXX: Beter way of storing this?
+    kind = models.ForeignKey(Kind)
     picture = models.CharField(max_length=20)
 
     def __unicode__(self):
@@ -28,15 +36,21 @@ class Player(models.Model):
     )
 
     gender = models.CharField(max_length=1, choices=GENDER)
+    opponent_kind = models.ForeignKey(Kind)
+    #opponents = models.ManyToManyField(Opponent)
+
+    # XXX: Express that all opponents should be of the same kind.
 
     def __unicode__(self):
-        return '<P(%s) %s>' % (self.kind, self.pk)
+        return '<P(%s) %s>' % (self.opponent_kind, self.pk)
 
 class Round(models.Model):
     player = models.ForeignKey(Player)
     opponent = models.ForeignKey(Opponent)
     amount_offered = models.IntegerField()
     accepted = models.BooleanField()
+
+    # XXX: Express that oppenent's kind matches player's selected opponent_kind.
 
     def __unicode__(self):
         return '<R(%s) %s / %s / %s>' % (self.accepted, self.opponent, this.amount_offered, this.player)
--- game/views.py
+++ game/views.py
@@ -2,21 +2,41 @@ import random
 
 from django.http import HttpResponseNotAllowed
 from django.shortcuts import render
+from django.views.decorators.http import require_GET, require_POST
 
-from game.models import Opponent, Player, Round
+from game.models import Kind, Opponent, Player, Round
 
+def get_or_create_player(session):
+    player, created = Player.objects.get_or_create(id=session.get('player_id', None),
+        defaults={'opponent_kind': random.choice(Kind.objects.all())})
+
+    if created:
+        session['player_id'] = player.id
+
+    return player
+
+@require_GET
 def start_game(request):
     return render(request, 'game/start_game.html', {})
 
+@require_POST
 def view_instructions(request):
-    # XXX: Add template explaining this is not the start of the game.
-    # XXX: We're going to repeat this often. Add nice decorator.
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'])
-
+    player = get_or_create_player(request.session)
     context = {
-      'opponent_kind': random.choice(Opponent.KIND)[0]
+      'opponent_kind': player.opponent_kind
     }
 
     return render(request, 'game/view_instructions.html', context)
+
+@require_POST
+def start_round(request):
+    return render(request, 'game/start_round.html', {})
+
+@require_POST
+def play_round(request):
+    return render(request, 'game/play_round.html', {})
+
+@require_POST
+def end_round(request):
+    return render(request, 'game/end_round.html', {})
 
