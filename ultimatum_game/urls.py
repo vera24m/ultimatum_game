cd8ddff05aa7e8ac14cfa13bb5a8542725bf5251
Vera Matei <vera24m@yahoo.com>
2013-10-20 20:55:18 +0200

    Remove example code from urls.py.

--- ultimatum_game/urls.py
+++ ultimatum_game/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ultimatum_game.views.home', name='home'),
-    # url(r'^ultimatum_game/', include('ultimatum_game.foo.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
