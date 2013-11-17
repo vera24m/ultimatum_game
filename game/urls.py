e81eb08c71daac8e18cac92d5dbfc65cdf574a0a
Maikel Steneker <maikel.steneker@gmail.com>
2013-11-17 22:55:48 +0100

    Added message for intentionality.

--- game/urls.py
+++ game/urls.py
@@ -5,6 +5,7 @@ from game import views
 urlpatterns = patterns(#'',
     url(r'^start/$', views.start_game, name='start_game'),
     url(r'^instructions/$', views.view_instructions, name='view_instructions'),
+    url(r'^intentionality/$', views.intentionality, name='intentionality'),
     url(r'^round/start/$', views.start_round, name='start_round'),
     url(r'^round/play/$', views.play_round, name='play_round'),
     url(r'^round/end/$', views.end_round, name='end_round'),
