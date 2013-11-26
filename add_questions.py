2bdbf7876f95c93be8de4d3ab223a013e2c3be4d
Maikel Steneker <maikel.steneker@gmail.com>
2013-11-26 20:59:21 +0100

    Restored questions script.

--- add_questions.py
+++ add_questions.py
@@ -34,13 +34,12 @@ add(second_batch_questions, second_batch_options)
 
 third_batch_questions = ['Overall, what is your attitude toward the opponents you encountered in this study?', 'Overall, how likeable did you find your opponents?', 'Overall, how attractive did you find the pictures of your opponents?', 'Overall, to what extent did you feel opponents 1 to 4 were responsible for the offers they made?', 'Overall, to what extent did you feel opponents 5 to 8 were responsible for the offers they made?', 'How noticeable were the differences between the opponents you encountered?',]
 
-#TODO: replace by sensible options for each question
-third_batch_options = [['Q1O1', 'Q1O2'],
-                       ['Q2O1', 'Q2O2'],
-                       ['etc'],
-                       ['etc'],
-                       ['etc'],
-                       ['etc'],
+third_batch_options = [['Extremely negative', 'Moderately negative', 'Slightly negative', 'Neither negative nor positive', 'Slightly positive', 'Moderately positive', 'Extremely positive',],
+                       ['Extremely unlikable', 'Moderately unlikable', 'Slightly unlikable', 'Neither unlikable nor likable', 'Slightly likeable', 'Moderately likeable', 'Extremely likeable',],
+                       ['Extremely unattractive', 'Moderately unattractive', 'Slightly unattractive', 'Neither unattractive nor attractive', 'Slightly attractive', 'Moderately attractive', 'Extremely attractive',],
+                       ['Extremely unresponsible', 'Moderately unresponsible', 'Slightly unresponsible', 'Neither unresponsible nor responsible', 'Slightly responsible', 'Moderately responsible', 'Extremely responsible',],
+                       ['Extremely unresponsible', 'Moderately unresponsible', 'Slightly unresponsible', 'Neither unresponsible nor responsible', 'Slightly responsible', 'Moderately responsible', 'Extremely responsible',],
+                       ['Extremely unnoticable', 'Moderately unnoticable', 'Slightly unnoticable', 'Neither unnoticable nor noticable', 'Slightly noticable', 'Moderately noticable', 'Extremely noticable',],
                       ]
 
 for x in range(len(third_batch_questions)):
@@ -48,26 +47,21 @@ for x in range(len(third_batch_questions)):
 
 fourth_batch_questions = ['Do you feel like your knowledge of the opponents\' appearance influenced your decisions in the game?', 'How motivated were you to earn as many Money Units as possible in the game?', 'Do you feel like emotions or other non-financial motivations influence your decisions in the game?',]
 
-#TODO: replace by sensible options for each question
-fourth_batch_options = [['Q1O1', 'Q1O2'],
-                       ['Q2O1', 'Q2O2'],
-                       ['etc'],
+fourth_batch_options = [['??', '??'], #TODO: replace by sensible options
+                       ['Extremely unmotivated', 'Moderately unmotivated', 'Slightly unmotivated', 'Neither unmotivated nor motivated', 'Slightly motivated', 'Moderately motivated', 'Extremely motivated',],
+                       ['etc'], #TODO: replace by sensible options
                       ]
 
 for x in range(len(fourth_batch_questions)):
     add(fourth_batch_questions[x:x+1], fourth_batch_options[x])
 
-fifth_batch_questions = ['On average, how many hours a day do you spend interacting with devices such as PC\'s, laptops, tablets and smart phones?', 'Do you have any personal experience with robots (including e.g. robotic toys like Furby and robotic appliances like vacuum cleaners or lawn mowers)?', 'The game you played in this experiment is called the \'Ultimatum Game\'. Had you ever heard of or played this game before?','Please indicate your gender.','Please indicate your age.','Please indicate your nationality.', 'Please indicate how religious you are.', 'Please indicate how spiritual you are.']
+fifth_batch_questions = ['Do you have any personal experience with robots (including e.g. robotic toys like Furby and robotic appliances like vacuum cleaners or lawn mowers)?', 'The game you played in this experiment is called the \'Ultimatum Game\'. Had you ever heard of or played this game before?','Please indicate your gender.', 'Please indicate how religious you are.', 'Please indicate how spiritual you are.']
 
-#TODO: replace by sensible options for each question
-fifth_batch_options = [['Q1O1', 'Q1O2'],
-                       ['Q2O1', 'Q2O2'],
-                       ['etc'],
-                       ['etc'],
-                       ['etc'],
-                       ['etc'],
-                       ['etc'],
-                       ['etc'],
+fifth_batch_options = [['Yes', 'No'],
+                       ['Yes', 'No'],
+                       ['Male', 'Female'],
+                       ['etc'], #TODO: replace by sensible options
+                       ['etc'], #TODO: replace by sensible options
                       ]
 
 for x in range(len(fifth_batch_questions)):
